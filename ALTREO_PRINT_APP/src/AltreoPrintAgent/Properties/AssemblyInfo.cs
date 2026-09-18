505ac9fd6aa8502cf25791d5037b3a60e6fb758d
Agahun <agahun02@gmail.com>
2026-09-18 08:43:28 +0200

    4.77 usuniecie salescenter

--- ALTREO_PRINT_APP/src/AltreoPrintAgent/Properties/AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("AltreoPrintAgent.Tests")]
--- orders.php
+++
@@ -1,5 +0,0 @@
-<?php
-/** Standalone order workspace, using the application's existing authentication. */
-declare(strict_types=1);
-$_GET['controller']='orders';
-require __DIR__.'/index.php';
